3992f0f4902dcf615e9b5c33067eeb33fc14cbf8
Michael Kelly <mkelly@mozilla.com>
2013-03-16 12:42:26 -0400

    Add local settings.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# Scribe
 Automatically create pages and upload images to a conflunce page.
+
+## Setup
+1. Copy `settings.py-dist` to `settings.py` and fill out the settings.
--- scribe.py
+++ scribe.py
@@ -1,1 +1,9 @@
+import os
+import sys
+
+
+# Add current directory to path so we can import settings.
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+import settings
+
 __version__ = '0.1'

709b61d10f64afed16f6e55234a4ec1c343efa46
Michael Kelly <mkelly@mozilla.com>
2013-03-16 14:57:06 -0400

    Make it work.

--- scribe.py
+++ scribe.py
@@ -17,56 +17,65 @@ import settings
 __version__ = '0.1'
 
 
-# Uploads an image to a given pageName
-def upload(parentPageName, pageName, imagepath):
+def upload(parent_name, page_name, image_path):
     """
-    Uploads an image to the confluence wiki page 'pageName' as an attachment, and inserts
-    a link + a thumbnail at the end of the wiki page content.  It also sets the parent page
-    to parentPageName.
+    Uploads an image to the confluence wiki page 'page_name' as an attachment,
+    and inserts a link + a thumbnail at the end of the wiki page content.  It
+    also sets the parent page to parent_name.
     """
-
-    filename = os.path.basename(imagepath)
+    filename = os.path.basename(image_path)
 
     # Login to the server
     server = xmlrpclib.ServerProxy(settings.SERVER)
     token = server.confluence2.login(settings.USERNAME, settings.PASSWORD)
 
     # Get the parent page(we need the numeric ID from it)
-    sprintPage = server.confluence2.getPage(token, settings.SPACE, parentPageName)
+    sprint_page = server.confluence2.getPage(token, settings.SPACE,
+                                             parent_name)
 
     # Try to get the page, but if it fails, that's ok, we'll create a new one
     try:
-        imagePage = server.confluence2.getPage(token, settings.SPACE, pageName)
+        image_page = server.confluence2.getPage(token, settings.SPACE,
+                                                page_name)
     except:
-        imagePage = {}
+        image_page = {}
 
     # Set all the page attributes
-    imagePage['space'] = settings.SPACE
-    imagePage['title'] = pageName
-    imagePage['parentId'] = sprintPage['id']
-    if 'content' not in imagePage:
-        imagePage['content'] = ""
+    image_page['space'] = settings.SPACE
+    image_page['title'] = page_name
+    image_page['parentId'] = sprint_page['id']
+    if 'content' not in image_page:
+        image_page['content'] = ''
 
-    # Append the proper formatted text to the wiki page to add the file we are about to upload
-    imageContent = "<p><ac:link><ri:attachment ri:filename=\"%s\" /></ac:link></p>" % filename
-    imageContent += "<p><ac:image ac:thumbnail=\"true\" ac:width=\"300\"><ri:attachment ri:filename=\"%s\" /></ac:image></p>" % filename
-    imagePage['content'] += imageContent
+    # Append the proper formatted text to the wiki page to add the file we are
+    # about to upload.
+    image_page['content'] += """
+      <p>
+        <ac:link><ri:attachment ri:filename="{filename}" /></ac:link>
+      </p>
+      <p>
+        <ac:image ac:thumbnail="true" ac:width="300">
+          <ri:attachment ri:filename="{filename}" />
+        </ac:image>
+      </p>
+    """.format(filename=filename)
 
     # Create or update the page that holds the images
-    imagePage = server.confluence2.storePage(token, imagePage)
+    image_page = server.confluence2.storePage(token, image_page)
 
     # Upload the image as an attachment to the page
     attachment = {}
-    with open(imagepath, "rb") as handle:
-        ba = xmlrpclib.Binary(handle.read())
+    with open(image_path, 'rb') as handle:
+        image_data = xmlrpclib.Binary(handle.read())
 
     attachment['comment'] = filename
     attachment['fileName'] = filename
-    attachment['contentType'] = "image/jpeg"
-    attachment['pageId'] = imagePage['id']
+    attachment['contentType'] = 'image/jpeg'
+    attachment['pageId'] = image_page['id']
 
     # actually make the call to upload the iamge
-    server.confluence2.addAttachment(token, imagePage['id'], attachment, ba)
+    server.confluence2.addAttachment(token, image_page['id'], attachment,
+                                     image_data)
 
 
 class PhotoEventHandler(FileSystemEventHandler):
@@ -75,27 +84,35 @@ class PhotoEventHandler(FileSystemEventHandler):
     on them.
     """
     def dispatch(self, event):
+        # Only handle file creation events.
+        if event.event_type != 'created':
+            return
+
         filename = os.path.basename(event.src_path)
         directory_name = os.path.basename(os.path.dirname(event.src_path))
         date = datetime.datetime.strptime(directory_name, '%Y-%m-%d').date()
 
         # Determine the sprint and page names based on the date.
         difference = settings.FIRST_SPRINT_DATE - date
-        sprint_name = 'Sprint {0}'.format((difference.days / 14) + 1)
+        sprint_name = 'Sprint {0}'.format(abs(difference.days / 14) + 1)
         page_name = 'Design Day {0} ({1})'.format(
-            difference.days % 14,
+            (difference.days % 14) + 1,
             date.strftime('%A %Y-%m-%d')
         )
 
+        msg = 'Uploading photo {filename} for {page_name} in {sprint_name}...'
+        print msg.format(filename=event.src_path, page_name=page_name,
+                         sprint_name=sprint_name)
         upload(sprint_name, page_name, event.src_path)
+        print 'Done'
 
         # Copy file to archive and delete the existing file.
-        destination = os.path.join(settings.ARCHIVE_DIRECTORY, directory_name,
-                                   filename)
-        shutil.move(event.src_path, destination)
+        dest_dir = os.path.join(settings.ARCHIVE_DIRECTORY, directory_name)
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+        shutil.move(event.src_path, os.path.join(dest_dir, filename))
 
 
-# Sample call: upload("Sprint 1", "Design Day 1", "/path/to/some/jpeg/2013-02-05_09-59-54_241.jpg")
 if __name__ == '__main__':
     handler = PhotoEventHandler()
     observer = Observer()

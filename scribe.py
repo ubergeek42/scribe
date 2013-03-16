2da0a592bfe7d7c204fe09b7b868d3efdfa9410c
Keith Johnson <kj@ubergeek42.com>
2013-03-16 15:10:37 -0400

    Create the sprint page if it doesn't exist
    Create the sprint page that is the parent of the design day page if it
    doesn't exist.
    Add a new setting to specify what the global parent page is

--- scribe.py
+++ scribe.py
@@ -30,8 +30,27 @@ def upload(parent_name, page_name, image_path):
     token = server.confluence2.login(settings.USERNAME, settings.PASSWORD)
 
     # Get the parent page(we need the numeric ID from it)
-    sprint_page = server.confluence2.getPage(token, settings.SPACE,
-                                             parent_name)
+    try:
+        sprint_page = server.confluence2.getPage(token, settings.SPACE,
+                                                 parent_name)
+    except:
+        # create the page if it didn't exist
+        sprint_parent = server.confluence2.getPage(token, settings.SPACE,
+                                                   settings.PARENT_PAGE)
+        sprint_page = {}
+        sprint_page['parentId'] = sprint_parent['id']
+        sprint_page['title'] = parent_name
+        sprint_page['space'] = settings.SPACE
+        sprint_page['content'] = """
+        <h3>Design :</h3>
+        <p>
+          <ac:macro ac:name="children">
+            <ac:parameter ac:name="depth">1</ac:parameter>
+          </ac:macro>
+        </p>
+        """
+        sprint_page = server.confluence2.storePage(token, sprint_page)
+
 
     # Try to get the page, but if it fails, that's ok, we'll create a new one
     try:
